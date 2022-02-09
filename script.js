27fff7ead345a04de572e39dd933fd2b849b437e
Bjørnar Øvstedal <bovstedal95@gmail.com>
2022-02-09 16:15:09 +0100

    Added content

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function cycleImage(){
-	delayInSeconds = 1;
+	delayInSeconds = 10;
 	var rotator = document.getElementById('rotator');
 	var imageDir = "images/";
 
 	var images = ["old_man_1", "old_man_2"];
 
 	var num = 0;
-	var changeImage = function () {
+	var changeImage = function(){
 		rotator.src = imageDir + images[num++] + ".jpg";
 		if (num == images.length){
 			num = 0;
@@ -15,8 +15,37 @@ function cycleImage(){
 	setInterval(changeImage, delayInSeconds * 1000);
 }
 
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting){
+			entry.target.classList.add('fadeInAnimation');
+			return;
+		}
+	});
+});
+
+observer.observe(document.querySelector('.quote p'));
+observer.observe(document.querySelector('.quote b'));
+observer.observe(document.querySelector('.information p'));
+observer.observe(document.querySelector('.wingding'));
+
+
+function cycleIcon(){
+	delayInSeconds = 2;
+	var num = 0;
+	var wingdings = ["&#11217", "&#9776", "&#9991", "&#8999", "&#8984", "&#11216"];
+	var changeIcon = function (){
+		document.getElementById("wingding_1").innerHTML = wingdings[num];
+		num++;
+		if (num == wingdings.length){
+			num = 0;
+		}
+	}
+	setInterval(changeIcon, delayInSeconds * 1000);
+}
 
 window.onload=function(){
 	cycleImage();
+	cycleIcon();
 	document.getElementById('rotator').src = ["images/old_man_1.jpg"];
 }
